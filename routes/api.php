bc5e96bc847a5e6c0228732c05a28093df250562
dipto <diptoguho4@gmail.com>
2025-11-20 00:58:30 +0600

    role

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\Authorization\RoleManageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Passport\Http\Middleware\CheckToken;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::middleware(['auth:admin-api', 'admin', CheckToken::using('admin')])->group(function () {
+  
+
+    Route::apiResource('/role', RoleManageController::class)->middleware('permission:role');
+    Route::get('roles/active', [RoleManageController::class, 'activeRoles']);
+    Route::get('/all-permissions', [RoleManageController::class, 'groupWisePermissions']);
+});
+
